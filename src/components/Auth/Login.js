43623008ca0a1a65c546da7222bf1f8ed7e65f56
Cassandra King <puddincat@gmail.com>
2020-10-15 17:21:32 -0400

    add handlesubmit, handlechhange, custom hook for validation

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,24 +1,43 @@
 import React, { useState }  from "react";
+import useFormValidation from './useFormValidation';
 
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+  password: ''
+}
 function Login(props) {
+  // destructure our props
+  const { handleSubmit, handleChange, values } = useFormValidation(INITIAL_STATE);
 
   const [login, setLogin] = useState(true);
 
   return (
     <div>
-    <h2 calssName="mv3">{login ? 'Login' : 'Create Account' }</h2>
-    <form className="flex flex-column">
-    {!login && <input
+    <h2 className="mv3">{login ? 'Login' : 'Create Account' }</h2>
+    <form onSubmit={handleSubmit} className="flex flex-column">
+    {!login && (
+      <input
+        onChange={handleChange}
+        value={values.name}
+        name="name" // nane property allows us to target each one as it updates
         type="text"
         placeholder="Your Name"
         autoComplete="off"
-    /> }
+    />
+    )}
     <input
+        onChange={handleChange}
+        value={values.email}
+        name="email"
         type="email"
         placeholder="Your Email"
         autoComplete="off"
     />
     <input
+        onChange={handleChange}
+        value={values.password}
+        name="password"
         type="password"
         placeholder="Choose secure password"
     />
--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function useFormValidation() {}
+function useFormValidation(initialState) {
+// our custom hook for validation
+const [values, setValues] = useState(initialState)
+  function handleChange(event) {
+    event.persist();
+    // use updater pattern when updating state based on previous states
+    setValues( previousValues => ({
+      ...previousValues, /// all previous values ' values '
+      [event.target.name]: event.target.value
+    }))
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log( {values })
+  }
+  // this is how we send the handleChange event to Login.js
+  return { handleSubmit, handleChange, values }
+}
 
 export default useFormValidation;
+// note on event.persist
+// If you want to access the event properties in an asynchronous way, you should call event.persist() on the event, which will remove the synthetic event from the pool and allow references to the event to be retained by user code.
